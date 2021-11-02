55b8e7009b8654846e00f9ba447541c706c179af
ErikMoxley <exilerzero@yahoo.com>
2021-11-01 22:37:00 -0400

    updated a bunch of stuff

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
+const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
 
 app.use(cors());
+app.use(bodyParser.urlencoded())
 
 var mysql = require('mysql')
 var connection = mysql.createConnection({
@@ -15,12 +18,34 @@ var connection = mysql.createConnection({
 
 connection.connect()
 
-app.get('/', (req, res, next) => {
-  connection.query('SELECT * from users', function (err, rows, fields) {
-    if (err) throw err
-  
-    res.send(rows);
-  })
+app.post('/',
+    body('email').isEmail({ max: 100 }),
+    body('name').isLength({ min: 1, max: 50 }),
+    body('industry').isLength({ min: 1, max: 100 }),
+    body('mobile').isLength({ min: 6, max: 20 }),
+    (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {name, email, mobile, industry} = req.body;
+
+    const sql = `INSERT INTO users
+    (
+        name, email, mobile, industry
+    )
+    VALUES
+    (
+        ?, ?, ?, ?
+    )`
+    connection.query(sql, 
+        [name, email, mobile, industry], 
+        function (err, data) {
+        if (err) throw err
+        res.send('Form Successfully Submitted!');
+    })
 })
 
 app.listen(port, () => {
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function getRequest () {
-    fetch('http://localhost:3000/')
-    .then(response => response.json())
-    .then(data => console.log(data));  
-}
+document.forms[0].onsubmit = (e) => {
+    e.preventDefault();
+    const params = new URLSearchParams([...new FormData(e.target).entries()]);
+    fetch("http://localhost:3000/", {
+        method:"POST"
+      , body:params
+      })
+      .then(res => res.text())
+      .then(alert)
+  };
