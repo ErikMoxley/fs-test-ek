fd7a5682b94ac712a14932e8cc38a4ed355214d7
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 14:56:13 -0500

    update

--- app.js
+++ app.js
@@ -10,7 +10,25 @@ app.use(cors({
     credentials: true
     })
 );
-headers.set("Access-Control-Allow-Origin", "https://fs-forms-ek.herokuapp.com");  
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://fs-forms-ek.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 
