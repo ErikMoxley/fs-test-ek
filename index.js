9ff49e682cea81f2781bed187e194b36b4d76e40
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 14:24:58 -0500

    testing

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ document.forms[0].onsubmit = (e) => {
     e.preventDefault();
     const params = new URLSearchParams([...new FormData(e.target).entries()]);
     fetch("https://fs-forms-ek.herokuapp.com", {
-        method:"POST",
         credentials: "include",
         body:params
       })
