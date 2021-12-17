04d5a50b345db00db392dfcac794380f26d39e31
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 12:36:50 -0500

    testing out app.use functions

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const port = process.env.PORT || 8000;
 const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 
-app.use(
-    cors({
-    origin: "*",
-    })
+app.use( function (req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    }
 );
 app.use(express.urlencoded({ extended: true }))
 
