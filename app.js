3c93e2dd7b25040a64d216c30cfa1487b44a6897
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 13:53:18 -0500

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors({
     origin: "https://fs-forms-ek.herokuapp.com/"
   }));
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://fs-forms-ek.herokuapp.com/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
