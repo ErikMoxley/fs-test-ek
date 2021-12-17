d97242dbcbe2992b1d579d74d914c017ef70fc3a
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 16:14:00 -0500

    testing new function w next

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const port = process.env.PORT || 8000;
 const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 
-app.use(cors({
-    origin: "https://fs-forms-ek.herokuapp.com/",
-    methods: 'POST',
-    credentials: true,    
-})
-);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+  app.get('/', function(req, res, next) {
+    // Handle the get for this route
+  });
+  
+  app.post('/', function(req, res, next) {
+   // Handle the post for this route
+  });
 
 app.use(express.urlencoded({ extended: true }))
 
