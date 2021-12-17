50f68472c5f801dfd77cabe5b8a95e999d9af70f
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 14:33:30 -0500

    update

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ document.forms[0].onsubmit = (e) => {
     e.preventDefault();
     const params = new URLSearchParams([...new FormData(e.target).entries()]);
     fetch("https://fs-forms-ek.herokuapp.com/", {
-        credentials: "include"
+        method:"POST"
+      , body:params
       })
       .then(res => res.text())
       .then(alert)
