c6f80f26e7fb957df8f01cee33026a6993e23865
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 15:10:00 -0500

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors({
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://fs-forms-ek.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
