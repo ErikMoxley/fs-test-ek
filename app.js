4d7ed4118497f2b2fe645da5cee1c75688e10e58
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 12:11:47 -0500

    added cors origin

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ const port = process.env.PORT || 8000;
 const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-    });
+app.use(cors({
+    origin: "https://fs-forms-ek.herokuapp.com/",
+    })
+);
 app.use(express.urlencoded({ extended: true }))
 
 var mysql = require('mysql')
