d678c0dfe53c73bcf5e9624643c00640d41fb3de
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 13:04:35 -0500

    testing cors

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const port = process.env.PORT || 8000;
 const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 
-app.use( function (req, res){
+app.use( function (req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");}
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();}
 );
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(cors()); // <---- cors middleware
 
 const mysql = require('mysql')
