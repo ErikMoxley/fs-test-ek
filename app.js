18851c440400670373dc682b51cb7ff50f25d63b
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 16:36:34 -0500

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://fs-forms-ek.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
