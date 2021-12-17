29a395cf0cd8da87c3dbebbf6261b85b09172ae8
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 13:16:36 -0500

    cors issue

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ document.forms[0].onsubmit = (e) => {
     e.preventDefault();
     const params = new URLSearchParams([...new FormData(e.target).entries()]);
     fetch("https://fs-forms-ek.herokuapp.com/", {
-        method:"POST"
-      , body:params
+        method:"POST",
+        credentials: "include",
+        body:params
       })
       .then(res => res.text())
       .then(alert)
