af5b140c3508a6d146179f15ced3df3ce4d025ab
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 16:41:29 -0500

    testing methods

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const { body, validationResult } = require('express-validator');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+ });
   
 app.use(express.urlencoded({ extended: true }))
 
