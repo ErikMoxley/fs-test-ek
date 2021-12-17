bd52f8610391cc961da8038ff557d46358b47367
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 12:42:09 -0500

    testing cors error fix

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
-const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 
-app.use( function (req, res, next){
+app.use( function (req, res){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
     }
 );
 app.use(express.urlencoded({ extended: true }))
