575aedac21f314955565e1738190c189c6b6788f
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 12:43:35 -0500

    testing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
+const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 
 app.use( function (req, res){
