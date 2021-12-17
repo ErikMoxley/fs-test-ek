e9e8c76bd90501ecf192fa2f4047221283f9c946
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 09:44:31 -0500

    port changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = 3000
 const cors = require('cors')
 const { body, validationResult } = require('express-validator');
 
