f032c647f170dc1a846a0de587ef914bf16dfef1
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 17:43:16 -0500

    revert

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'https://fs-forms-ek.herokuapp.com');
+    res.header("Access-Control-Allow-Origin", "https://fs-forms-ek.herokuapp.com");
     res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+ });
   
 app.use(express.urlencoded({ extended: true }))
 
