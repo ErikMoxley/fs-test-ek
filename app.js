6c2d9f13528a7c8688fb089795aff22719c6de19
ErikMoxley <exilerzero@yahoo.com>
2021-12-17 14:52:06 -0500

    update

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(cors({
     credentials: true
     })
 );
+headers.set("Access-Control-Allow-Origin", "https://fs-forms-ek.herokuapp.com");  
 
 app.use(express.urlencoded({ extended: true }));
 
