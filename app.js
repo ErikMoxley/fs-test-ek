e1e349bc9d508d43cfa677f9825a34cac8e344a5
ErikMoxley <exilerzero@yahoo.com>
2021-12-18 11:53:15 -0500

    testing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { body, validationResult } = require('express-validator');
 const cors = require('cors');
 
 app.use(cors({
-    origin: "https://erikmoxley.github.io/fs-test-ek",
+    origin: "https://erikmoxley.github.io/fs-test-ek/",
     methods: ["POST"],
     })
 );
