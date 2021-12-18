edbde003311fa17264add704b616c3aa1a626bf1
ErikMoxley <exilerzero@yahoo.com>
2021-12-18 09:33:33 -0500

    removed forward slash on access-control-allow-origin url

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post('/',
 })
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://erikmoxley.github.io/fs-test-ek/");
+    res.header("Access-Control-Allow-Origin", "https://erikmoxley.github.io/fs-test-ek");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
