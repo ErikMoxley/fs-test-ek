064213eb832f4e9aad1a4e929457ab89500e6cd9
ErikMoxley <exilerzero@yahoo.com>
2021-12-18 09:27:44 -0500

    moved functions around

--- app.js
+++ app.js
@@ -4,15 +4,7 @@ const port = process.env.PORT || 8000;
 const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 
-app.use(cors, function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://erikmoxley.github.io/fs-test-ek/");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
- });
-  
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 const mysql = require('mysql')
 const connection = mysql.createConnection({
@@ -21,7 +13,9 @@ const connection = mysql.createConnection({
   password: '5b0ec773',
   database: 'heroku_e26755c34a76256'
 })
-connection.connect()
+connection.connect();
+
+app.use(cors());
 
 app.post('/',
     body('email').isEmail({ max: 100 }),
@@ -49,6 +43,14 @@ app.post('/',
         res.send('Form Successfully Submitted!');
     })
 })
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://erikmoxley.github.io/fs-test-ek/");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ });
 
 app.listen(port, () => {
     console.log("App is running on port " + port);
