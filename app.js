fc3ba994e03e1273b7cd0bf30f9b285391d72e94
ErikMoxley <exilerzero@yahoo.com>
2021-12-18 12:40:09 -0500

    testing:

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { body, validationResult } = require('express-validator');
 app.use(cors());
 
 app.use(function(req, res, next) {  
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', 'https://erikmoxley.github.io/fs-test-ek');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 }); 
