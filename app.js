9a32a8a4e5ae6fcad47160b6968e54159dbd36c1
ErikMoxley <exilerzero@yahoo.com>
2021-12-19 09:30:48 -0500

    redid connection to mysql

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const cors = require('cors')
 const { body, validationResult } = require('express-validator');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }))
+
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'b56589ccf620a5',
+  password: '5b0ec773',
+  database: 'heroku_e26755c34a76256'
+})
+
+connection.connect()
 
 app.post('/',
     body('email').isEmail({ max: 100 }),
     body('name').isLength({ min: 1, max: 50 }),
-    body('industry').isLength({ min: 1, max: 100 }),
     body('mobile').isLength({ min: 6, max: 20 }),
+    body('industry').isLength({ min: 1, max: 100 }),
     (req, res) => {
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
+
     const {name, email, mobile, industry} = req.body;
+
     const sql = `INSERT INTO users
     (
         name, email, mobile, industry
@@ -32,16 +47,6 @@ app.post('/',
     })
 })
 
-const mysql = require('mysql')
-const connection = mysql.createConnection({
-  host: 'us-cdbr-east-05.cleardb.net',
-  user: 'b56589ccf620a5',
-  password: '5b0ec773',
-  database: 'heroku_e26755c34a76256'
-})
-connection.connect();
-
 app.listen(port, () => {
-    console.log("App is running on port " + port);
-});
-
+  console.log(`Listening at http://localhost:${port}`)
+})
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.forms[0].onsubmit = (e) => {
   e.preventDefault();
   const params = new URLSearchParams([...new FormData(e.target).entries()]);
-  fetch("https://fs-forms-ek.herokuapp.com", {
+  fetch("http://localhost:3000/", {
       method:"POST"
     , body:params
     })
